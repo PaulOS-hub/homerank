f94325538efbfcae45e9bedc82054444de19f37b
unknown <292817809@qq.com>
2021-09-09 16:40:21 +0800

    创建项目，引入antD

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Button } from 'antd-mobile';
 function App() {
   return (
     <div className="App">
@@ -9,14 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+        <Button>default</Button>
       </header>
     </div>
   );
